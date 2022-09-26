461dea01ef383d8912841d0373cfcff29ae26058
TeChRF24 <f.rafael.temich24@gmail.com>
2022-09-26 11:17:06 -0500

    Testing a function to be executed on form component

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes  from 'prop-types';
 export const AddCategory = ({ onNewCategory }) => {
-    const [inputValue, setInputValue] = useState('Flubber');
+    const [inputValue, setInputValue] = useState('');
     const onInputChanged = ({target}) => {
         setInputValue(target.value);
     }
--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -12,12 +12,28 @@ describe('Tests on <AddCategory/>', () => {
     });
     test('It must call onNewCategory if input has a value', () => {
         const inputValue = 'Manchester City';
-        render(<AddCategory onNewCategory={ () => {} }/>);
+        const onNewCategory = jest.fn();
+
+        render(<AddCategory onNewCategory={ onNewCategory }/>);
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
         fireEvent.input( input, { target: { value: inputValue } });
         fireEvent.submit( form );
         expect( input.value ).toBe('');
+
+        expect( onNewCategory ).toHaveBeenCalled();  
+        expect( onNewCategory ).toHaveBeenCalledTimes(1);
+        expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+        
         // screen.debug();
+    });
+    test('It must not call onNewCategory if input is empty', () => {
+        const onNewCategory = jest.fn();
+
+        render(<AddCategory onNewCategory={ onNewCategory }/>);
+        const form = screen.getByRole('form');
+        fireEvent.submit( form );
+        expect( onNewCategory ).toHaveBeenCalledTimes( 0 );
+        expect( onNewCategory ).not.toHaveBeenCalled();
     });
 });
